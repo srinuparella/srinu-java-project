32e25a85c2e913a88da02c59cca105bda69adaae
srinuparella <srinuparella.tech@gmail.com>
2025-10-31 19:48:04 +0530

    javaProject

---
+++ src/test/java/com/example/demo/HomeControllerTest.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHomePageLoads() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk());
+    }
+}
